0978c7d25fdcf47df833174ec19a20279b136e01
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-02-07 23:18:45 -0600

    test staging

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+    const requestData = req;
+    console.log(requestData);
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ name: 'John Doe2' }))
