ce242b9cd334215f0354622f89dcb9bbfd9cc7b3
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-02-08 20:18:09 -0600

    added server runtime config

--- next.config.js
+++ next.config.js
@@ -60,6 +60,11 @@ module.exports = withPlugins([
       MONGODB_URI: process.env.MONGODB_URI,
       DB_NAME: process.env.DB_NAME,
     },
+    serverRuntimeConfig: {
+      // Will only be available on the server side
+      MONGODB_URI: process.env.MONGODB_URI,
+      DB_NAME: process.env.DB_NAME,
+    },
   }
 );
 
