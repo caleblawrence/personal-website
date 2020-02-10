e36c5a9810d14de216d0fba4bb6b961074bf6de9
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-02-09 18:37:08 -0600

    hard coded db uri

--- middleware/database.js
+++ middleware/database.js
@@ -2,7 +2,10 @@ import { MongoClient } from 'mongodb';
 
 console.log("mongo URI", process.env.mongodb_uri);
 
-const client = new MongoClient(process.env.mongodb_uri, {
+// env variables in now are too hard
+let mongodb_uri = 'mongodb+srv://caleblawrence:testpass@personalwebsite-6wydn.mongodb.net/test?retryWrites=true&w=majority';
+
+const client = new MongoClient(mongodb_uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -11,6 +14,6 @@ const client = new MongoClient(process.env.mongodb_uri, {
 export default async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
-  req.db = client.db(process.env.DB_NAME);
+  req.db = client.db('hello');
   return next();
 }
