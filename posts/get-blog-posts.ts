bd2a8e9988de30c7ee1d36a6582d89c3a0171916
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-02-11 20:35:35 -0600

    switched last file to ts

--- posts/get-blog-posts.ts
+++ posts/get-blog-posts.ts
@@ -1,16 +1,16 @@
-const fs = require('fs')
-const path = require('path')
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
 
-const DIR = path.join(process.cwd(), '/pages/posts/')
+
+const DIR = join(process.cwd(), '/pages/posts/')
 const META = /export\s+const\s+meta\s+=\s+({[\s\S]*?\n})/
-const files = fs
-  .readdirSync(DIR)
-  .filter(file => file.endsWith('.md') || file.endsWith('.mdx'))
+const files = readdirSync(DIR)
+  .filter((file: string) => file.endsWith('.md') || file.endsWith('.mdx'))
 
 module.exports = files
   .map((file, index) => {
-    const name = path.join(DIR, file)
-    const contents = fs.readFileSync(name, 'utf-8')
+    const name = join(DIR, file)
+    const contents = readFileSync(name, 'utf-8')
     const match = META.exec(contents)
 
     if (!match || typeof match[1] !== 'string') {
@@ -29,4 +29,4 @@ module.exports = files
     }
   })
   .filter(meta => meta.published)
-  .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
+  .sort((a, b) => +new Date(b.publishedAt) - +new Date(a.publishedAt))
