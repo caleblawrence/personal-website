038d019acea53a14b5d378c008e1c858c3a74edf
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-05-22 17:40:35 -0500

    fixed code block width bug

--- components/Blog/postBody.tsx
+++ components/Blog/postBody.tsx
@@ -7,6 +7,16 @@ export default function PostBody({ content }) {
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
+      <style jsx>{`
+            pre {
+              white-space: pre-wrap !important;       /* Since CSS 2.1 */
+              white-space: -moz-pre-wrap !important;  /* Mozilla, since 1999 */
+              white-space: -pre-wrap !important;      /* Opera 4-6 */
+              white-space: -o-pre-wrap !important;    /* Opera 7 */
+              word-wrap: break-word !important;       /* Internet Explorer 5.5+ */
+            }
+
+      `}</style>
     </div>
   )
 }
