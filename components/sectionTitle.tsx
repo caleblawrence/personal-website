46e16278716ce4a9ed890aadcf31d1d3f99deb30
Caleb Lawrence <caleblawrence@MacBook-Pro.local>
2020-06-22 23:16:39 -0500

    format file

--- components/sectionTitle.tsx
+++ components/sectionTitle.tsx
@@ -1,14 +1,14 @@
-
 interface Props {
-  title: string
+  title: string;
 }
 
 export default function SectionTitle(props: Props) {
   const { title } = props;
   return (
-    <h2 className="mb-0 text-6xl md:text-8xl font-bold tracking-tighter leading-tight" style={{fontFamily: "Helvetca, Arial, sans-serif"}}>
+    <h2
+      className="mb-0 text-6xl md:text-8xl font-bold tracking-tighter leading-tight"
+      style={{ fontFamily: 'Helvetca, Arial, sans-serif' }}>
       {title}
     </h2>
   );
 }
-
