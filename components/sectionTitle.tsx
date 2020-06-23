a493dcaa02f57ea83862983c1f6a3fd7703aaae9
Caleb Lawrence <caleblawrence@MacBook-Pro.local>
2020-06-22 23:14:13 -0500

    fixed section title font for windows

--- components/sectionTitle.tsx
+++ components/sectionTitle.tsx
@@ -1,8 +1,14 @@
 
-export default function SectionTitle({title}) {
-  return (
-    <h2 className="mb-0 text-6xl md:text-8xl font-bold tracking-tighter leading-tight">
-    {title}
-    </h2>
-  )
+interface Props {
+  title: string
 }
+
+export default function SectionTitle(props: Props) {
+  const { title } = props;
+  return (
+    <h2 className="mb-0 text-6xl md:text-8xl font-bold tracking-tighter leading-tight" style={{fontFamily: "Helvetca, Arial, sans-serif"}}>
+      {title}
+    </h2>
+  );
+}
+
