095ce6585903b2a1f9513e0553e03fcee6b7979f
Caleb Lawrence <caleblawrence@MacBook-Pro.beambox>
2020-06-27 07:32:19 -0500

    added signify health to work section

--- components/work.tsx
+++ components/work.tsx
@@ -5,23 +5,55 @@ const Work = () => (
   <div className="mt-10">
     <SectionTitle title="Work." />
 
-    <div className="muvWork">
-      <div className="muvColorOverlay">
-        <div className="workContent">
-          <h1 className="companyTitle">muv</h1>
-          <img src="/assets/images/muv-logo.png" className="companyLogo" alt="muv logo" />
-          <p className="workDescription">As a software developer at muv I worked on software design, backend, and frontend development.</p>
+    <div className="twoColumn">
+      <div className="signifyWork">
+        <div className="signifyColorOverlay">
+          <div className="workContent">
+            <h1 className="companyTitle">Signify Health</h1>
+            <img
+              src="/assets/images/signify-health-logo.png"
+              className="companyLogo"
+              alt="signify logo"
+            />
+            <p className="workDescription">
+              At Signify I'm learning how to build microservices using .NET
+              Core and Kafka.
+            </p>
+          </div>
         </div>
       </div>
-
+      <div className="muvWork">
+        <div className="muvColorOverlay">
+          <div className="workContent">
+            <h1 className="companyTitle">muv</h1>
+            <img
+              src="/assets/images/muv-logo.png"
+              className="companyLogo"
+              alt="muv logo"
+            />
+            <p className="workDescription">
+              As a software developer at muv I worked on software design,
+              backend, and frontend development using Laravel and React.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
     <div className="twoColumn">
       <div className="atosWork">
         <div className="atosColorOverlay">
           <div className="workContent">
             <h1 className="companyTitle">Atos</h1>
-            <img src="/assets/images/atos-logo.png" className="companyLogo" alt="muv logo" />
-            <p className="workDescription">I worked with Atos for my senior design project in college. My team built a custom survey application that Atos uses to determine the needs of potential clients.</p>
+            <img
+              src="/assets/images/atos-logo.png"
+              className="companyLogo"
+              alt="muv logo"
+            />
+            <p className="workDescription">
+              I worked with Atos for my senior design project in college. My
+              team built a custom survey application that Atos uses to determine
+              the needs of potential clients.
+            </p>
           </div>
         </div>
       </div>
@@ -29,8 +61,15 @@ const Work = () => (
         <div className="developingSolutionsColorOverlay">
           <div className="workContent">
             <h1 className="companyTitle">Developing Solutions</h1>
-            <img src="/assets/images/developing-solutions-logo.png" className="companyLogo" alt="muv logo" />
-            <p className="workDescription">I interned at Developing Solutions for a couple years while in school and worked on multiple in-house web applications.</p>
+            <img
+              src="/assets/images/developing-solutions-logo.png"
+              className="companyLogo"
+              alt="muv logo"
+            />
+            <p className="workDescription">
+              I interned at Developing Solutions for a couple years while in
+              school and built multiple in-house web applications using Django, Node.js, and React.
+            </p>
           </div>
         </div>
       </div>
@@ -52,6 +91,7 @@ const Work = () => (
             }
             .atosColorOverlay {
                 background-color: rgba(0,101,158, 0.925);
+                height: 100%;
             }
             .developingSolutionsColorOverlay {
                 background-color: rgba(42,44,46, 0.925);
@@ -66,12 +106,27 @@ const Work = () => (
             }
             .muvColorOverlay {
                 background-color: rgba(42,44,46, 0.8);
+                height: 100%;
             }
             .muvWork {
                 color: white;
                 background-image: url(/assets/images/muv-banner.jpg);
                 background-size:cover;
                 background-repeat:no-repeat;
+                flex: 1;
+            }
+
+            .signifyColorOverlay {
+                background-color: rgba(71,129,137, 0.8);
+                height: 100%;
+            }
+
+             .signifyWork {
+                color: white;
+                background-image: url(/assets/images/signify-banner.jpg);
+                background-size:cover;
+                background-repeat:no-repeat;
+                flex: 1;
             }
             .companyTitle {
                 text-align: center;
