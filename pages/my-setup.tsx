d819bfd2ef542ee8cc228b3836a891ac1a3034c9
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-08-27 10:58:02 -0500

    updated my setup page to look better

--- pages/my-setup.tsx
+++ pages/my-setup.tsx
@@ -1,12 +1,19 @@
 import HomeButton from 'components/HomeButton';
 import Layout from 'components/layout';
+import Head from 'next/head';
+import Container from 'components/container';
 
 export default function MySetup() {
   return (
     <>
       <Layout>
-        <HomeButton />
-        <img src="/assets/desk.jpg" className="me" alt="me" />
+        <Head>
+          <title>Uses</title>
+        </Head>
+        <Container>
+          <HomeButton />
+          <img src="/assets/desk.jpg" className="me" alt="me" style={{maxHeight: 1000}} />
+        </Container>
       </Layout>
     </>
   );
